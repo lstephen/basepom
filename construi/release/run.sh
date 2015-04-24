26d01532eb9f405d1dd1df47f1cddab54a79fee8
lstephen <levi.stephen@gmail.com>
2015-04-24 15:21:59 -0700

    Update release script

--- construi/release/run.sh
+++ construi/release/run.sh
@@ -2,21 +2,36 @@
 
 set -e
 
-git status
+git config user.name $GIT_AUTHOR_NAME
+git config user.email $GIT_AUTHOR_EMAIL
 
-# Remove -SNAPSHOT version version
+release_commit=`git rev-parse HEAD`
+
+echo "Releasing commit ${release_commit}..."
+
+# Remove -SNAPSHOT from version
 master_version=`mvn -B help:evaluated -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$" | sed 's/\(.*\)-SNAPSHOT/\1/'`
 
-mvn versions:set -DnewVersion=$master_version
+echo "Release version is ${master_version}"
+mvn -B versions:set -DnewVersion=$master_version
+
+echo "Pushing to master..."
 git commit -m "Update master version to $master_version"
 git push origin master
+echo "Push to master done."
 
-git checkout $GIT_COMMMIT
-mvn release:update-version
+echo "Updating development version..."
+git checkout $release_commit
+mvn -B release:update-version
 
 develop_version=`mvn -B help:evaluated -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$"`
 
+echo "New development version is $develop_version"
+
+echo "Pushing to develop..."
 git commit -m "Update develop version to $develop_version"
 git push origin develop
+echo "Push to develop done."
 
+echo "Release done."
 
