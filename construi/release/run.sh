161704a351604fda4ff9218ff5c4dbfef9d8f270
lstephen <levi.stephen@gmail.com>
2015-04-24 15:14:03 -0700

    Updates to release process

--- construi/release/run.sh
+++ construi/release/run.sh
@@ -2,8 +2,21 @@
 
 set -e
 
-gpg --import /root/.gnupg/public.key
-gpg --import --allow-secret-key-import /root/.gnupg/private.key
 git status
-mvn -B -e -X -DstartCommit=$GIT_COMMIT -DpushReleases=ture jgitflow:release-start jgitflow:release-finish
+
+# Remove -SNAPSHOT version version
+master_version=`mvn -B help:evaluated -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$" | sed 's/\(.*\)-SNAPSHOT/\1/'`
+
+mvn versions:set -DnewVersion=$master_version
+git commit -m "Update master version to $master_version"
+git push origin master
+
+git checkout $GIT_COMMMIT
+mvn release:update-version
+
+develop_version=`mvn -B help:evaluated -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$"`
+
+git commit -m "Update develop version to $develop_version"
+git push origin develop
+
 
