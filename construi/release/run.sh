cadca5aa2f3252abd832176d5c87803dd42c1c40
lstephen <levi.stephen@gmail.com>
2015-04-24 16:21:26 -0700

    Update run script

--- construi/release/run.sh
+++ construi/release/run.sh
@@ -15,26 +15,36 @@ echo "Releasing commit ${release_commit}..."
 master_version=`mvn -B help:evaluate -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$" | sed 's/\(.*\)-SNAPSHOT/\1/'`
 
 echo "Release version is ${master_version}"
+
+release_branch=release/$master_version
+git checkout -b $release_branch
+
 mvn -B versions:set -DnewVersion=$master_version -DgenerateBackupPoms=false
 
 echo "Pushing to master..."
 git add pom.xml
 git commit -m "Update master version to $master_version"
-git push origin master
+git push origin $release_branch:master
 echo "Push to master done."
 
 echo "Updating development version..."
 git checkout $release_commit
-mvn -B release:update-version
 
 develop_version=`mvn -B help:evaluate -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$"`
 
-echo "New development version is $develop_version"
+develop_branch=develop/$develop_version
+git checkout -b $develop_branch
+
+mvn -B release:update-versions
+
+new_develop_version=`mvn -B help:evaluate -Dexpression=project.version | grep "^[^\s]*-SNAPSHOT$"`
+
+echo "New development version is $new_develop_version"
 
 echo "Pushing to develop..."
 git add pom.xml
-git commit -m "Update develop version to $develop_version"
-git push origin develop
+git commit -m "Update develop version from $develop_version to $mew_develop_version"
+git push origin $develop_branch:develop
 echo "Push to develop done."
 
 echo "Release done."
