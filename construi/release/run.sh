421748bc4dcd88582464fc0df905889b06840d78
lstephen <levi.stephen@gmail.com>
2015-04-24 16:10:18 -0700

    Update release script

--- construi/release/run.sh
+++ construi/release/run.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-git config user.name $GIT_AUTHOR_NAME
-git config user.email $GIT_AUTHOR_EMAIL
+[[ -n "$GIT_AUTHOR_NAME" ]] && git config user.name $GIT_AUTHOR_NAME
+[[ -n "$GIT_AUTHOR_EMAIL" ]] && git config user.email $GIT_AUTHOR_EMAIL
+
+printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 release_commit=`git rev-parse HEAD`
 
