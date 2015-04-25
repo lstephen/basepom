e5a369391e44fe91a3cb8a386c43762c079c168b
lstephen <levi.stephen@gmail.com>
2015-04-24 17:15:37 -0700

    Update publish script

--- construi/publish/run.sh
+++ construi/publish/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+[[ -n "$GIT_AUTHOR_NAME" ]] && git config user.name $GIT_AUTHOR_NAME
+[[ -n "$GIT_AUTHOR_EMAIL" ]] && git config user.email $GIT_AUTHOR_EMAIL
+
+printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config 
+
 commit=`git rev-parse HEAD`
 version=`mvn -B help:evaluate -Dexpression=project.version | grep "^[\d\.]*$"`
 tag=v$version
--- construi/release/run.sh
+++ construi/release/run.sh
@@ -9,7 +9,7 @@ set -e
 [[ -n "$GIT_AUTHOR_NAME" ]] && git config user.name $GIT_AUTHOR_NAME
 [[ -n "$GIT_AUTHOR_EMAIL" ]] && git config user.email $GIT_AUTHOR_EMAIL
 
-printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config 
 
 release_commit=`git rev-parse HEAD`
 
